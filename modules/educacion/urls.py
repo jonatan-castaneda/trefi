435d7afcbbd606e89ed5afee2c07fb1f12a8d687
Ian Castañeda <jonatan_ian@outlook.com>
2017-06-01 20:12:07 -0500

    Educación track

--- modules/educacion/urls.py
+++ modules/educacion/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import url
-from .views import index, contenido, contenido_categoria, contenido_busqueda
+from .views import *
 
 urlpatterns = [
     url(r'^$', index, name="index"),
     url(r'^(?P<pk>[0-9]+)/$',contenido, name="contenido"),
     url(r'^categoria/(?P<pk>[a-zA-Z]+)/$', contenido_categoria, name="contenido_categoria"),
     url(r'^busqueda/$', contenido_busqueda, name="contenido_busqueda"),
+    url(r'^cursos/$', cursos, name="cursos"),
+    url(r'^cursos/(?P<pk>[0-9]+)/$', cursos_ver, name="cursos"),
+    url(r'^cursos/agregar/$', cursos_agregar, name="cursos_agregar"),
+    url(r'^cursos/agregar/(?P<pk>[0-9]+)/$', cursos_registrar, name="curso_registrar"),
+    url(r'^cursos/eliminar/(?P<pk>[0-9]+)/$', cursos_eliminar, name="curso_eliminar"),
 ]
--- modules/educacion/views.py
+++ modules/educacion/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Contenido, CATEGORIA, TEMA
+from django.shortcuts import render, redirect
+from .models import Contenido, CATEGORIA, TEMA, Curso, TrackingEducacion
+
 import json
 
 # Create your views here.
@@ -46,7 +47,7 @@ def contenido_categoria(request, pk=""):
             'tema':tem,
             })
         except:
-            return index(request)
+            return redirect("educacion:index")
 
 def contenido_busqueda(request):
     if request.method == "GET":
@@ -61,7 +62,58 @@ def contenido_busqueda(request):
             'tema':tem,
             })
         except:
-            return index(request)
+            return redirect("educacion:index")
     else:
-        return index(request)
+        return redirect("educacion:index")
 
+def cursos(request):
+    try:
+        categ = dict(CATEGORIA)
+        cursos = TrackingEducacion.objects.filter(usuario = request.user)
+        return render(request, "educacion/curso.html", {
+            'categoria':categ,
+            'cursos':cursos,
+        })
+    except:
+        return render(request,"educacion/curso.html", {
+            'sin_cursos':"Aún no tienes cursos registrados",
+        })
+    #except:
+    #    return redirect("educacion:index")
+
+def cursos_agregar(request, pk=0):
+    cursos = Curso.objects.all()
+    return render(request, "educacion/add_cursos.html",{
+        'cursos':cursos,
+    })
+
+def cursos_registrar(request, pk=0):
+    try:
+        print(pk)
+        curso = TrackingEducacion()
+        curso.usuario = request.user
+        curso.curso = Curso.objects.get(id=pk)
+        curso.save()
+        return redirect("educacion:cursos")
+    except:
+        return redirect("educacion:cursos")
+
+def cursos_eliminar(request, pk=0):
+    try:
+        TrackingEducacion.objects.get(id=pk).delete()
+        return redirect("educacion:cursos")
+    except:
+        return redirect("educacion:cursos")
+
+def cursos_ver(request, pk=0):
+    try:
+        cursos_user = TrackingEducacion.objects.get(id = pk)
+        categ = dict(CATEGORIA)
+        tem = dict(TEMA)
+        return render(request, "educacion/ver_curso.html",{
+            'categoria':categ,
+            'tema':tem,
+            'cursos':cursos_user.curso.contenidos.all(),
+        })
+    except:
+        return redirect("educacion:cursos")
